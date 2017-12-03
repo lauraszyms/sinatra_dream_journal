736a79ae59a19850be7949610a0c6a5a88c9fd80
lauraszyms <lauraszyms@gmail.com>
2017-12-03 14:18:20 -0800

    Added / route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < Sinatra::Base
-configure do
-set :public_folder, 'public'
-set :views, 'app/views'
-enable :sessions
-set :session_secret, "password_security"
-end
-get '/' do
-"Hello, World!"
-end
+    configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  get "/" do
+   erb :index
+  end
+
+
 end
