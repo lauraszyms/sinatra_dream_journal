2da9ad902144dacff85afa7c285f4b2ea09bc6a1
lauraszyms <lauraszyms@gmail.com>
2017-12-03 20:08:03 -0800

    split routes into seperate controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,57 @@
 class UsersController < ApplicationController
 
+  get "/signup" do
+    if !User.exists?(session[:user_id])
+      erb :'/users/signup'
+    else
+      @user = current_user
+      redirect "/users/#{current_user.id}"
+   end
+  end
 
+  post "/signup" do
+    if params[:username] == "" || params[:password] == ""
+      redirect "/signup"
+    else
+      @user = User.new(:username => params[:username], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect "/users/#{@user.id}"
+    end
+  end
 
+  get '/users/:id' do
+    @user = User.find_by(params[:id])
+    erb :'/users/profile'
+  end
 
+  get "/login" do
+    if !User.exists?(session[:user_id])
+      erb :'/users/login'
+    else
+      redirect "/dreams"
+    end
+  end
+
+  post "/login" do
+   @user = User.find_by(:username => params[:username])
+
+   if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/users/#{@user.id}"
+   else
+      redirect "/signup"
+   end
+  end
+
+  get "/logout" do
+    if !User.exists?(session[:user_id])
+      redirect "/"
+    else
+      session.clear
+      redirect "/login"
+    end
+  end
 
 
 
